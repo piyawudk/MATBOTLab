c39e06b1e009835e9ac1e8597450d51b6037015c
Piyawud Koonmanee <piyawudkoon@hotmail.com>
2023-11-07 14:46:07 +0900

    update location

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import App from './app/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
+  <BrowserRouter basename="/lab/">
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
